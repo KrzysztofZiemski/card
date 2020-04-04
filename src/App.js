6ed6b2b2e0a849c339ac02c6d53f601516d526fd
Krzysztof Ziemski <krzysztofziemski@wp.pl>
2020-04-04 19:46:06 +0200

    add animated library fix some bugs sass HomePage

--- src/App.js
+++ src/App.js
@@ -2,90 +2,90 @@ import React, { useRef } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainNavbar from './components/MainNavbar/MainNavbar';
 import HomePage from './templates/HomePage/HomePage';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 import './App.scss';
+//npm install react-animate-on-scroll --save
 
 function App() {
-  const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+
+  const scrollBack = (ref, back) => {
+
+  }
+
+  const scrollToRef = (ref) => {
+    for (let i = 1; i <= ref.current.offsetTop + 500; i++) {
+      setTimeout(() => {
+        window.scrollTo(0, i)
+      }, (i + 5) * .2);
+
+    }
+  };
   let homePageRef = useRef(null);
   let aboutPageRef = useRef(null);
   let stackPageRef = useRef(null);
   let projectPageRef = useRef(null);
-  // handleScrollToElement(event) {
-  //   const tesNode = ReactDOM.findDOMNode(this.refs.test)
-  //   if (some_logic){
-  //     window.scrollTo(0, tesNode.offsetTop);
-  //   }
-  // }
-
-  // render() {
-
-  //   return (
-  //     <div>
-  //       <div ref="test"></div>
-  //     </div>)
-  // }
 
   return (
     <Router>
       <div className="App">
-        <section className="pages">
-          <header className="pages-mainNav">
-            <MainNavbar scrollToRef={scrollToRef} pagesRef={{ homePageRef, aboutPageRef, stackPageRef, projectPageRef }} />
-          </header>
-          <Switch>
-            <Route path='/' >
-              <section ref={homePageRef}>
-                <HomePage />
-              </section>
-              <section ref={aboutPageRef}>
+        <header className="pages-mainNav">
+          <MainNavbar scrollToRef={scrollToRef} pagesRef={{ homePageRef, aboutPageRef, stackPageRef, projectPageRef }} />
+        </header>
+        <Switch>
+          <Route path='/' >
+            <section ref={homePageRef}>
+              <HomePage />
+            </section>
+            <section ref={aboutPageRef}>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <var> <p>sekcja 2</p></var>
+              <var> <p>sekcja 2</p></var>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <var> <p>sekcja 2</p></var>
+              <var> <p>sekcja 2</p></var>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-                <p>sekcja 2</p>
-                <p>sekcja 2</p>
+              <p>sekcja 2</p>
+              <p>sekcja 2</p>
 
-              </section>
+            </section>
+            <ScrollAnimation animateIn="fadeIn">
               <section ref={stackPageRef}>
 
                 <p>sekcja 3</p>
@@ -151,48 +151,48 @@ function App() {
                 <p>sekcja 3</p>
                 <p>sekcja 3</p>
               </section>
-              <section ref={projectPageRef}>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-                <p>sekcja 4</p>
-              </section>
+            </ScrollAnimation>
+            <section ref={projectPageRef}>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+              <p>sekcja 4</p>
+            </section>
 
-            </Route>
-          </Switch>
-        </section>
-        <aside className="App-blank" />
+          </Route>
+        </Switch>
+
       </div>
     </Router>
   );
--- src/templates/HomePage/HomePage.js
+++ src/templates/HomePage/HomePage.js
@@ -8,21 +8,24 @@ const HomePage = (props, ref) => {
 
     return (
         <div className='homePage'>
-            <nav className='asideNav'>
-                <ul className='asideNav-list'>
-                    <li className='asideNav-list-element'> <a href="https://www.linkedin.com/in/krzysztof-ziemski"><img src={linkedinIcon} alt='Linkedin icon' /></a></li>
-                    <li className='asideNav-list-element'> <a href="http://github.com/KrzysztofZiemski"><img src={githubIcon} alt='Github icon' /></a></li>
-                    <li className='asideNav-list-element asideNav-list-element-text'> <a href="https://www.linkedin.com/in/krzysztof-ziemski">View Profile</a></li>
-                </ul>
-            </nav>
-            <main className='homePage-main'>
-                <div className='headers'>
-                    <h1 className='headers-header-h1'>Krzysztof Ziemski</h1>
-                    <h2 className='headers-header-h2'>Junior Front-end Developer</h2>
-                </div>
-                <a href="http://github.com/KrzysztofZiemski" className='homePage-main-icon'><img src={githubIcon} alt='Github icon' /></a>
-                <img src={profile} alt="photos Krzysztof Ziemski" className='homePage-main-profileImage' />
-            </main>
+            <div className='homePage-main'>
+                <nav className='asideNav'>
+                    <ul className='asideNav-list'>
+                        <li className='asideNav-list-element'> <a href="https://www.linkedin.com/in/krzysztof-ziemski"><img src={linkedinIcon} alt='Linkedin icon' /></a></li>
+                        <li className='asideNav-list-element'> <a href="http://github.com/KrzysztofZiemski"><img src={githubIcon} alt='Github icon' /></a></li>
+                        <li className='asideNav-list-element asideNav-list-element-text'> <a href="https://www.linkedin.com/in/krzysztof-ziemski">View Profile</a></li>
+                    </ul>
+                </nav>
+                <main className='homePage-main'>
+                    <div className='headers'>
+                        <h1 className='headers-header-h1'>Krzysztof Ziemski</h1>
+                        <h2 className='headers-header-h2'>Junior Front-end Developer</h2>
+                    </div>
+                    <a href="http://github.com/KrzysztofZiemski" className='homePage-main-icon'><img src={githubIcon} alt='Github icon' /></a>
+                    <img src={profile} alt="photos Krzysztof Ziemski" className='homePage-main-profileImage' />
+                </main>
+            </div>
+            <div className='homePage-aside'><div></div><div></div></div>
         </div>
     )
 }
