cc2d7d7e082529fe72a1e34417fb0979dc7732af
Krzysztof Ziemski <krzysztofziemski@wp.pl>
2020-04-15 23:59:56 +0200

    styled navbar + add new color to design

--- src/App.js
+++ src/App.js
@@ -16,24 +16,6 @@ import './App.scss';
 function App() {
   let [lang, setLang] = useState(langAvaible.pl);
 
-  const scrollToRef = (ref) => {
-    let distance = ref.current.offsetTop - window.pageYOffset;
-
-    if (distance >= 0) {
-      for (let i = window.pageYOffset; i <= ref.current.offsetTop; i++) {
-        setTimeout(() => {
-          window.scrollTo(0, i)
-        }, (i + 5) * .2);
-      }
-    } else {
-      for (let i = window.pageYOffset; i >= ref.current.offsetTop; i--) {
-        console.log(i)
-        setTimeout(() => {
-          window.scrollTo(0, i)
-        }, (i - 400) * .2);
-      }
-    }
-  };
 
   let homePageRef = useRef(null);
   let aboutPageRef = useRef(null);
@@ -48,7 +30,7 @@ function App() {
           <SelectLang setLang={setLang} lang={lang} />
         </div>
         <header className="App__mainNav" ref={headerRef}>
-          <MainNavbar scrollToRef={scrollToRef} lang={lang} pagesRef={{ homePageRef, aboutPageRef, projectPageRef, experiencePageRef, coursesPage }} />
+          <MainNavbar lang={lang} pagesRef={{ homePageRef, aboutPageRef, projectPageRef, experiencePageRef, coursesPage }} />
         </header>
         <Switch>
           <Route path='/' >
--- src/components/MainNavbar/MainNavbar.js
+++ src/components/MainNavbar/MainNavbar.js
@@ -1,34 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './MainNavbar.scss';
 import burgerIcon from '../../assets/burger-icon.svg';
 import BurgerButton from '../BurgerButton/BurgerButton';
 import { mainNavbar } from '../../content.json';
 
-const MainNavbar = ({ scrollToRef, pagesRef, lang }) => {
+const MainNavbar = ({ pagesRef, lang }) => {
     const content = mainNavbar[lang];
     const { homePageRef, aboutPageRef, projectPageRef, experiencePageRef, coursesPage } = pagesRef;
 
-    let [hideMenu, setHideMenu] = useState(true);
+    let [menuActive, setMenuActive] = useState(false);
+    let [pageY, setPageY] = useState(0);
 
+    useEffect(() => {
+        window.addEventListener('scroll', handleBgcNavBar);
+        return (window.addEventListener('scroll', handleBgcNavBar))
+    })
+    const handleBgcNavBar = () => {
+        setPageY(window.pageYOffset);
+    }
+
+    const scrollToRef = (ref) => {
+        let distance = ref.current.offsetTop - window.pageYOffset;
+
+        if (distance >= 0) {
+            for (let i = window.pageYOffset; i <= ref.current.offsetTop; i++) {
+                setTimeout(() => {
+                    window.scrollTo(0, i)
+                }, (i + 5) * .2);
+            }
+        } else {
+            for (let i = window.pageYOffset; i >= ref.current.offsetTop; i--) {
+                console.log(i)
+                setTimeout(() => {
+                    window.scrollTo(0, i)
+                }, i * -1);
+            }
+        }
+    };
+
+    const getClassesMenu = () => {
+        let classes = 'mainNavContainer';
+        if (menuActive) classes = classes + ' show';
+        if (pageY >= 500) classes = classes + ' bgc';
+        return classes;
+
+    }
+    const showMenu = () => {
+        setMenuActive(true);
+    }
+    const hideMenu = () => {
+        setMenuActive(false);
+    }
     return (
-        <nav className='mainNavContainer'>
-            <ul className="mainNavbar">
-                <li className='mainNavbar__listElement mainNavbar__listElement--burger'>
-                    <BurgerButton onClick={() => setHideMenu(!hideMenu)}><img src={burgerIcon} alt="menu-icon" /></BurgerButton>
-                </li>
-                <li className='mainNavbar__listElement hide'><Link to='/' onClick={() => scrollToRef(homePageRef)}>{content.homeLink}</Link></li>
-                <li className='mainNavbar__listElement hide'><Link to='/about' onClick={() => scrollToRef(aboutPageRef)}>{content.aboutLink}</Link></li>
-                <li className='mainNavbar__listElement hide'><Link to='/courses' onClick={() => scrollToRef(coursesPage)}>{content.coursesLink}</Link></li>
-                <li className='mainNavbar__listElement hide'><Link to='/jobs' onClick={() => scrollToRef(experiencePageRef)}>{content.jobsLink}</Link></li>
-                <li className='mainNavbar__listElement'><Link to='/projects' onClick={() => scrollToRef(projectPageRef)}>{content.projectsLink}</Link></li>
-            </ul>
-            <ul className={hideMenu ? 'slideNavbar hide' : 'slideNavbar'}>
-                <li className='slideNavbar__listElement'><Link to='/' onClick={() => { scrollToRef(homePageRef); setHideMenu(!hideMenu) }}>{content.homeLink}</Link></li>
-                <li className='slideNavbar__listElement'><Link to='/about' onClick={() => { scrollToRef(aboutPageRef); setHideMenu(!hideMenu) }}>{content.aboutLink}</Link></li>
-                <li className='slideNavbar__listElement'><Link to='/projects' onClick={() => { scrollToRef(projectPageRef); setHideMenu(!hideMenu) }}>{content.projectsLink}</Link></li>
-                <li className='slideNavbar__listElement'><Link to='/courses' onClick={() => { scrollToRef(coursesPage); setHideMenu(!hideMenu) }}>{content.coursesLink}</Link></li>
-                <li className='slideNavbar__listElement'><Link to='/jobs' onClick={() => { scrollToRef(experiencePageRef); setHideMenu(!hideMenu) }}>{content.jobsLink}</Link></li>
+        <nav className={getClassesMenu()}>
+            <BurgerButton className={pageY < 500 ? 'burgerBtn' : 'burgerBtn bgc'} onClick={menuActive ? hideMenu : showMenu}><img src={burgerIcon} alt="menu-icon" /></BurgerButton>
+            <ul className={menuActive ? 'slideNavbar' : 'slideNavbar hide'}>
+                <li className='slideNavbar__listElement'><Link to='/' onClick={() => { scrollToRef(homePageRef); hideMenu() }}>{content.homeLink}</Link></li>
+                <li className='slideNavbar__listElement'><Link to='/about' onClick={() => { scrollToRef(aboutPageRef); hideMenu() }}>{content.aboutLink}</Link></li>
+                <li className={pageY < 500 ? 'slideNavbar__listElement slideNavbar__listElement--alwaysShow' : 'slideNavbar__listElement slideNavbar__listElement--alwaysShow bgc'}><Link to='/projects' onClick={() => { scrollToRef(projectPageRef); hideMenu() }}>{content.projectsLink}</Link></li>
+                <li className='slideNavbar__listElement'><Link to='/courses' onClick={() => { scrollToRef(coursesPage); hideMenu() }}>{content.coursesLink}</Link></li>
+                <li className='slideNavbar__listElement'><Link to='/jobs' onClick={() => { scrollToRef(experiencePageRef); hideMenu() }}>{content.jobsLink}</Link></li>
             </ul>
         </nav >
     )
