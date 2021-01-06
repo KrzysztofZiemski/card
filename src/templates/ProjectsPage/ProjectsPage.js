2b7b0d5f70ebee81e679a9d24161e6be36647ac1
Krzysztof Ziemski <krzysztofziemski@wp.pl>
2021-01-06 23:06:42 +0100

    add 2 projects

--- src/templates/ProjectsPage/ProjectsPage.js
+++ src/templates/ProjectsPage/ProjectsPage.js
@@ -6,6 +6,8 @@ import { projectsPage } from '../../content.json';
 import shopProjectIMG from '../../assets/shopView.png';
 import bookProjectIMG from '../../assets/bookView.png';
 import cardProjectIMG from '../../assets/cardView.png';
+import zliczkaProjectIMG from '../../assets/zliczkaView.png';
+import blankProjectIMG from '../../assets/blankMonitor.png';
 // , linkGitHub
 const ProjectsPage = ({ lang }) => {
     const content = projectsPage[lang];
@@ -13,18 +15,27 @@ const ProjectsPage = ({ lang }) => {
     return (
         <div className="projectsPage">
             <h1 className='projectsPage__header'>{content.title}</h1>
-            {/* ////////////////////////////////////////////// */}
             <ScrollAnimation animateOnce={true} animateIn='fadeIn'>
                 <Project
-                    img={cardProjectIMG}
-                    name={content.projects.card.name}
-                    stack={[content.stacks.html, content.stacks.css, content.stacks.sass, content.stacks.js, content.stacks.rwd, content.stacks.react]}
-                    description={content.projects.card.description}
-                    linkLive={content.projects.card.linkLive}
-                    linkGitHub={content.projects.card.linkGitHub}
+                    img={zliczkaProjectIMG}
+                    name={content.projects.zliczka.name}
+                    stack={[content.stacks.html, content.stacks.material, content.stacks.js, content.stacks.react]}
+                    description={content.projects.zliczka.description}
+                    linkLive={content.projects.zliczka.linkLive}
+                    linkGitHub={content.projects.zliczka.linkGitHub}
                 ></Project>
             </ScrollAnimation>
-            {/* ////////////////////////////////////////////// */}
+            <ScrollAnimation animateOnce={true} animateIn='fadeIn'>
+                <Project
+                    img={blankProjectIMG}
+                    name={content.projects.hextl.name}
+                    stack={[content.stacks.html, content.stacks.material, content.stacks.js, content.stacks.ts, content.stacks.webpack]}
+                    description={content.projects.hextl.description}
+                    linkLive={content.projects.hextl.linkLive}
+                    linkGitHub={content.projects.hextl.linkGitHub}
+                ></Project>
+            </ScrollAnimation>
+
             <ScrollAnimation animateOnce={true} animateIn='fadeIn'>
                 <Project
                     img={cardProjectIMG}
